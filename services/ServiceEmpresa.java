58d3d53c2bd816f76d556c227e32c842f299fe65
Vinicius Rezende <viniciusarantes133@gmail.com>
2023-10-06 14:10:35 -0300

    ServiceEmpresa e ServiceUniversidade finalizados

---
+++ services/ServiceEmpresa.java
@@ -0,0 +1,173 @@
+package services;
+
+import java.util.List;
+
+import models.Empresa;
+import repositories.RepositoryEmpresa;
+import util.Utils;
+
+public class ServiceEmpresa extends ServiceInstituicao implements IService {
+    private RepositoryEmpresa repositoryEmpresa = RepositoryEmpresa.getInstance();
+
+    public void menuInicial() {
+        int optionInt = 0;
+
+        do {
+            if (scan.nextLine() == "\n" || optionInt == 0) {
+                Utils.clearTerminal();
+                Utils.printMenuTitle("Menu do Empresa");
+                System.out.println("1 - cadastrar Empresa");
+                System.out.println("2 - Deletar Empresa");
+                System.out.println("3 - atualizar Empresa");
+                System.out.println("4- Buscar Empresa");
+                System.out.println("5 - Mostrar todos os Empresas");
+                System.out.println("6 - Voltar para o menu principal");
+            }
+
+            String option = scan.nextLine();
+            try {
+                optionInt = Integer.parseInt(option);
+            } catch (Exception e) {
+                System.out.println("Opção inválida!");
+                System.out.println("Pressione enter para continuar...");
+            }
+            if (optionInt > 6) {
+                System.out.println("Opção inválida!");
+                System.out.println("Pressione enter para continuar...");
+            }
+
+            switch (optionInt) {
+                case 1:
+                    register();
+
+                    break;
+
+                case 2:
+                    delete();
+                    break;
+
+                case 3:
+                    update();
+                    break;
+
+                case 4:
+                    querryById();
+                    break;
+                case 5:
+                    querryAll();
+                    break;
+
+                default:
+                    break;
+            }
+            if (optionInt != 6) {
+                optionInt = 0;
+            }
+        } while (optionInt != 6);
+    }
+
+    public void register() {
+        repositoryEmpresa.addElement(getEmpresaInfo());
+    }
+
+    private Empresa getEmpresaInfo() {
+        Empresa newEmpresa = new Empresa();
+        newEmpresa.setId(Long.valueOf((repositoryEmpresa.getAmountOfElements() + 1)));
+        getInstituicaoInfo(newEmpresa);
+        System.out.println("Informe o ramo");
+        newEmpresa.setRamo(scan.nextLine());
+        return newEmpresa;
+    }
+
+    public void delete() {
+        Utils.clearTerminal();
+        System.out.println("Informe o id do Empresa");
+        String option = scan.nextLine();
+        Long optionLong = Long.parseLong("0");
+        try {
+            optionLong = Long.parseLong(option);
+            Empresa Empresa = getEmpresa(optionLong);
+            if (Empresa == null) {
+                System.out.println("Empresa não encontrado");
+            } else {
+                Boolean result = repositoryEmpresa.deleteElement(Empresa);
+                if (result) {
+                    System.out.println("Empresa Deletado com sucesso");
+                } else {
+                    System.out.println("Problema no Sistema, tente novamente mais tarde!");
+                }
+
+            }
+
+        } catch (Exception e) {
+            System.out.println("Opção inválida!");
+            delete();
+        }
+
+    }
+
+    public void update() {
+        Utils.clearTerminal();
+        System.out.println("Informe o id do Empresa");
+        String option = scan.nextLine();
+        Long optionLong = Long.parseLong("0");
+        try {
+            optionLong = Long.parseLong(option);
+            Empresa Empresa = getEmpresa(optionLong);
+            if (Empresa == null) {
+                System.out.println("Empresa não encontrado");
+            } else {
+                Empresa newEmpresa = getEmpresaInfo();
+                newEmpresa.setId(Empresa.getId());
+                Boolean result = repositoryEmpresa.updateElement(optionLong, newEmpresa);
+                if (result) {
+                    System.out.println("Empresa Atualizado com sucesso");
+                } else {
+                    System.out.println("Problema no Sistema, tente novamente mais tarde!");
+                }
+
+            }
+        } catch (Exception e) {
+            System.out.println("Opção inválida!");
+            update();
+        }
+
+    }
+
+    public void querryById() {
+        Utils.clearTerminal();
+        System.out.println("Informe o id do Empresa");
+        String option = scan.nextLine();
+        Long optionLong = Long.parseLong("0");
+        try {
+            optionLong = Long.parseLong(option);
+            Empresa Empresa = getEmpresa(optionLong);
+            if (Empresa == null) {
+                System.out.println("Empresa não encontrado");
+            } else {
+                System.out.println(Empresa.toString());
+            }
+
+        } catch (Exception e) {
+            System.out.println("Opção inválida!");
+            querryById();
+        }
+
+    }
+
+    private Empresa getEmpresa(Long id) {
+        return repositoryEmpresa.findElementById(id);
+    }
+
+    public void querryAll() {
+        List<Empresa> Empresas = repositoryEmpresa.findAllElements();
+        if (Empresas.isEmpty()) {
+            System.out.println("Nenhum Empresa cadastrado!");
+        } else {
+            Empresas.forEach(Empresa -> {
+                System.out.println(Empresa.toString());
+            });
+        }
+
+    }
+}
---
+++ services/ServiceUniversidade.java
@@ -0,0 +1,186 @@
+package services;
+
+import java.util.List;
+
+import models.Universidade;
+import repositories.RepositoryUniversidade;
+import util.Utils;
+
+public class ServiceUniversidade extends ServiceInstituicao implements IService {
+    private RepositoryUniversidade repositoryUniversidade = RepositoryUniversidade.getInstance();
+
+    public void menuInicial() {
+        int optionInt = 0;
+
+        do {
+            if (scan.nextLine() == "\n" || optionInt == 0) {
+                Utils.clearTerminal();
+                Utils.printMenuTitle("Menu do Universidade");
+                System.out.println("1 - cadastrar Universidade");
+                System.out.println("2 - Deletar Universidade");
+                System.out.println("3 - atualizar Universidade");
+                System.out.println("4- Buscar Universidade");
+                System.out.println("5 - Mostrar todos os Universidades");
+                System.out.println("6 - Voltar para o menu principal");
+            }
+
+            String option = scan.nextLine();
+            try {
+                optionInt = Integer.parseInt(option);
+            } catch (Exception e) {
+                System.out.println("Opção inválida!");
+                System.out.println("Pressione enter para continuar...");
+            }
+            if (optionInt > 6) {
+                System.out.println("Opção inválida!");
+                System.out.println("Pressione enter para continuar...");
+            }
+
+            switch (optionInt) {
+                case 1:
+                    register();
+
+                    break;
+
+                case 2:
+                    delete();
+                    break;
+
+                case 3:
+                    update();
+                    break;
+
+                case 4:
+                    querryById();
+                    break;
+                case 5:
+                    querryAll();
+                    break;
+
+                default:
+                    break;
+            }
+            if (optionInt != 6) {
+                optionInt = 0;
+            }
+        } while (optionInt != 6);
+    }
+
+    public void register() {
+        repositoryUniversidade.addElement(getUniversidadeInfo());
+    }
+
+    private Universidade getUniversidadeInfo() {
+        Universidade newUniversidade = new Universidade();
+        newUniversidade.setId(Long.valueOf((repositoryUniversidade.getAmountOfElements() + 1)));
+        getInstituicaoInfo(newUniversidade);
+        System.out.println("Informe o Reitor");
+        newUniversidade.setReitor(scan.nextLine());
+        registerCursos(newUniversidade);
+        return newUniversidade;
+    }
+
+    public void registerCursos(Universidade universidade) {
+        String cargo;
+        do {
+            Utils.clearTerminal();
+            System.out.println("Informe o curso desejado(para sair escrava finalizar)");
+            cargo = scan.nextLine();
+            if (cargo.toLowerCase() != "finalizar") {
+                universidade.addCargos(cargo);
+            }
+        } while (cargo.toLowerCase() != "finalizar");
+    }
+
+    public void delete() {
+        Utils.clearTerminal();
+        System.out.println("Informe o id do Universidade");
+        String option = scan.nextLine();
+        Long optionLong = Long.parseLong("0");
+        try {
+            optionLong = Long.parseLong(option);
+            Universidade Universidade = getUniversidade(optionLong);
+            if (Universidade == null) {
+                System.out.println("Universidade não encontrado");
+            } else {
+                Boolean result = repositoryUniversidade.deleteElement(Universidade);
+                if (result) {
+                    System.out.println("Universidade Deletado com sucesso");
+                } else {
+                    System.out.println("Problema no Sistema, tente novamente mais tarde!");
+                }
+
+            }
+
+        } catch (Exception e) {
+            System.out.println("Opção inválida!");
+            delete();
+        }
+
+    }
+
+    public void update() {
+        Utils.clearTerminal();
+        System.out.println("Informe o id do Universidade");
+        String option = scan.nextLine();
+        Long optionLong = Long.parseLong("0");
+        try {
+            optionLong = Long.parseLong(option);
+            Universidade Universidade = getUniversidade(optionLong);
+            if (Universidade == null) {
+                System.out.println("Universidade não encontrado");
+            } else {
+                Universidade newUniversidade = getUniversidadeInfo();
+                newUniversidade.setId(Universidade.getId());
+                Boolean result = repositoryUniversidade.updateElement(optionLong, newUniversidade);
+                if (result) {
+                    System.out.println("Universidade Atualizado com sucesso");
+                } else {
+                    System.out.println("Problema no Sistema, tente novamente mais tarde!");
+                }
+
+            }
+        } catch (Exception e) {
+            System.out.println("Opção inválida!");
+            update();
+        }
+
+    }
+
+    public void querryById() {
+        Utils.clearTerminal();
+        System.out.println("Informe o id do Universidade");
+        String option = scan.nextLine();
+        Long optionLong = Long.parseLong("0");
+        try {
+            optionLong = Long.parseLong(option);
+            Universidade Universidade = getUniversidade(optionLong);
+            if (Universidade == null) {
+                System.out.println("Universidade não encontrado");
+            } else {
+                System.out.println(Universidade.toString());
+            }
+
+        } catch (Exception e) {
+            System.out.println("Opção inválida!");
+            querryById();
+        }
+
+    }
+
+    private Universidade getUniversidade(Long id) {
+        return repositoryUniversidade.findElementById(id);
+    }
+
+    public void querryAll() {
+        List<Universidade> Universidades = repositoryUniversidade.findAllElements();
+        if (Universidades.isEmpty()) {
+            System.out.println("Nenhum Universidade cadastrado!");
+        } else {
+            Universidades.forEach(Universidade -> {
+                System.out.println(Universidade.toString());
+            });
+        }
+
+    }
+}
